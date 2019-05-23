850353bc69c150401c94e492dfc4364fc4d99e75
josuampotech <josua.marcelc@gmail.com>
2019-05-23 17:48:16 +0800

    Update title and description

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Just Learn Something New
+          Just Learn React JS In Philippines Community
         </p>
       </header>
     </div>
